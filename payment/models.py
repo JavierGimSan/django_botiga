54c14e637f67f2cd8ca89ad383359cb34faf79d2
JavierGimSan <javi.gimsa@gmail.com>
2025-02-26 20:20:47 +0100

    Creació d'aplicacions catalog i payment

---
+++ payment/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Payment(models.Models):
+    numero_tarjeta = models.CharField(max_length=20)
+    data_caducitat = models.DateField()
+    CVC = models.CharField(max_length=3)
+    estat_pagament = models.BooleanField(default=False)
