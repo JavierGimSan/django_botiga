45e94da2faccf81b2ed79abdd9f338f2093bdd74
JavierGimSan <javi.gimsa@gmail.com>
2025-03-03 22:44:32 +0100

    Canvis a settings, catalog y payment

--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-
-class Product(models.Models):
+class Product(models.Model):
     nom = models.CharField(max_length=100)
     descripcio = models.CharField(max_length=1000)
     preu = models.DecimalField(max_digits=10, decimal_places=2)
--- payment/models.py
+++ payment/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Payment(models.Models):
+class Payment(models.Model):
     numero_tarjeta = models.CharField(max_length=20)
     data_caducitat = models.DateField()
     CVC = models.CharField(max_length=3)
